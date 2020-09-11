8cb3227df16559729e7020cc3412ffe4f3fd8c9d
kabdelka <ksabdelka@gmail.com>
2020-09-11 12:19:11 +0100

    Responsive design

--- Responsive Design/index.js
+++ Responsive Design/index.js
@@ -1,2 +1,15 @@
-let button = document.querySelector('.but')
-button.addEventListener("click",()=> {console.log('you are dreaming')});
+let btn = document.querySelector('.fa-bars')
+let btnClose = document.querySelector('.fa-times')
+let sidebar = document.querySelector('.sidebar')
+
+btn.addEventListener('click',()=>{
+	sidebar.setAttribute('class','sidebar');
+})
+
+btnClose.addEventListener('click', ()=> {
+	sidebar.setAttribute('class','sidebar translate');
+})
+
+window.addEventListener('resize',()=> {
+	sidebar.setAttribute('class','sidebar translate')
+})
